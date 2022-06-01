1db30b7dda259e848d7eecffdb3e8632e5acda33
G4ldin0 <94557905+G4ldin0@users.noreply.github.com>
2022-06-01 07:44:58 -0300

    questao 3 lab 26

--- primeiroeultimo.cpp
+++ primeiroeultimo.cpp
@@ -3,7 +3,7 @@
 #include <cstring>
 using namespace std;
 
-void firstname(char origem[], char destino[])
+void firstname(const char origem[], char destino[])
 {
 	int i = 0;
 	for (i; origem[i] != ' '; i++)
@@ -13,30 +13,33 @@ void firstname(char origem[], char destino[])
 	destino[i] = '\0';
 }
 
-void lastname(char origem[], char destino[])
+void lastname(const char origem[], char destino[])
 {
-	int i = strlen(origem)- 1;
-	int j = 0;
+	int i = 0;
+	int j = strlen(origem)-1;
+	int conta{};
+	for (j; origem[j-1] != ' '; j--) 
+		++conta; //conta quantos caracteres tem no ultimo nome
 
-	for (i; origem[i] != ' '; i--)
+	for (i, j; i <= conta; i++, j++)
 	{
-		destino[strlen(origem) - i] = origem[i];
-
+		destino[i] = origem[j]; //anexa os caracteres dessa posição contada até o fim
 	}
-	destino[strlen(origem)-i + 1] = '\0';
+	destino[i] = '\0'; //no final adiciona o \0
 }
 
 int main()
 {
-	char nome[50], primeiro[50], ultimo[50];
+	char nome[50], primeiro[15], ultimo[15];
 
-	cout << "Entre com o nome: ";
-	cin.getline(nome, 50);
-	firstname(nome, primeiro);
-	cout << primeiro;
-
-	cout << ", ";
-
-	lastname(nome, ultimo);
-	cout << ultimo;
+	cout << "Entre com os nomes a cadastrar ( \'.\' para encerrar): \n";
+	for (cin.getline(nome, 50); strcmp(nome, "."); cin.getline(nome, 50))
+	{
+		firstname(nome, primeiro);
+		lastname(nome, ultimo);
+		cout << ultimo << ", " << primeiro << endl;
+		
+		primeiro[0] = '\0'; //pra evitar futuros erros
+		ultimo[0] = '\0';
+	}
 }
